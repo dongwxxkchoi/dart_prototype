851607b5e3ca18efb7b6e7d1f6083c6e8b56bb24
dongwxxkchoi <dwgygygy@bside.ai>
2023-10-26 16:41:44 +0900

    First Commit

---
+++ pages/1_🧑‍🤝‍🧑_executives.py
@@ -0,0 +1,4 @@
+# Libraries
+import streamlit as st
+
+st.set_page_config(page_title='임원목록', page_icon=':bar_chart:', layout='wide')
---
+++ pages/2_💰_shareholders.py
@@ -0,0 +1,4 @@
+# Libraries
+import streamlit as st
+
+st.set_page_config(page_title='지분관계', page_icon=':bar_chart:', layout='wide')
